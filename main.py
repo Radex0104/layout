df68ecc051f215176ae3128c9c6d168702de7c45
Daniil <skuratov.danya@gmail.com>
2024-12-15 16:47:18 +0300

    Revert "4 лаба"
    This reverts commit 52f511d7

--- main.py
+++ main.py
@@ -5,119 +5,89 @@
 йцукен и diktor с использованием 2-х файлов: voina-i-mir и 1grams-3.
 """
 
-import asyncio
 from text_analyzer import TextAnalyzer
+from visualization import *
 from time import monotonic
-
-from visualization import draw_histogram_combo
-
-filename = r'C:\1grams-3.txt'
-labels = ('йцукен', 'diktor', 'zubachew', 'skoropis')
-symbols_with_shift = (('!', '"', '№', ';', '%', ':', '?',
-                       '*', '(', ')', '_', '+', '/', ','),
-                      ('№', '%', ':', ';', '-', '"', '(',
-                       ')', '+', 'ъ', '?', '!', '_'),
-                      ('!', '"', '№', ';', '%', ':', '?',
-                       '*', '(', ')', '_', '+', '/'),
-                      ('',))
-homerows = (('ф', 'ы', 'в', 'а', 'о', 'л', 'д', 'ж', ' '),
-            ('у', 'и', 'е', 'о', 'н', 'т', 'с', 'р', ' '),
-            ('г', 'и', 'е', 'о', 'т', 'с', 'н', 'з', ' '),
-            ('у', 'и', 'е', 'о', 'н', 'т', 'с', 'р', ' '),
-            (' ',))
-keylout_dd = {'rfi1б': [(' ',),
-                        (' ',),
-                        (' ',),
-                        (' ',)],
-              'rfi5м': [('+', '_', ')', '-', '=', 'з', 'х',
-                         'ъ', 'ж', 'э'),
-                        ('+', '_', ')', '0', '*', '=', 'ч',
-                         'ш', 'щ', 'р', 'й', 'ж'),
-                        ('0', ')', '-', '_', '=', '+', 'х',
-                         'ц', 'щ', '/', 'з', 'ж', 'ч'),
-                        (')', '_', '"',
-                         'ч', 'ш', 'щ', 'р', 'й', 'ж')],
-              'rfi4б': [('(', '0', 'щ', 'д', 'ю', '.'),
-                        ('(', '9', 'д', 'с', 'г'),
-                        ('9', '(', 'п', 'н', 'к'),
-                        ('(', 'д', 'с', 'г')],
-              'rfi3с': [('*', '8', '9', 'ш', 'л', 'б'),
-                        ('"', '8', 'к', 'т', 'п'),
-                        ('8', '*', 'р', 'с', 'в'),
-                        ('к', 'т', 'п')],
-              'rfi2у': [('6', '?', '7', 'н', 'г', 'р', 'о',
-                         'т', 'ь'),
-                        ('6', '-', ';', '7', 'з', 'в', 'л',
-                         'н', 'м', 'б'),
-                        ('7', '?', '6', ':', 'й', 'л', 'б',
-                         'м', 'т', 'д'),
-                        ('-', 'з', 'в', 'л', 'н', 'б', 'м')],
-              'lfi2у': [('4', '%', '5', 'к', 'е', 'а', 'п',
-                         'м', 'и'),
-                        ('4', '5', '.', ',', '!', '?', 'а',
-                         'ы', 'ю', 'о'),
-                        ('5', '4', '%', ';', ',', '.', 'я',
-                         'о', 'у', 'э'),
-                        ('?', '!', ',', '.', 'о', 'а',
-                         'ы', 'ю')],
-              'lfi3с': [('№', '3', 'у', 'в', 'с'),
-                        ('3', '№', 'я', 'е', 'х'),
-                        ('3', '№', 'а', 'е', 'ю'),
-                        ('ъ', 'я', 'е', 'х')],
-              'lfi4б': [('"', '2', 'ц', 'ы', 'ч'),
-                        ('2', 'ь', 'ъ', 'и', 'э'),
-                        ('2', '"', 'ы', 'и', 'ь', 'ъ'),
-                        ('ё', 'ь', 'и', 'э')],
-              'lfi5м': [('!', 'ё', 'й', 'ф', 'я', '1'),
-                        ('ё', '1', 'ц', 'у', 'ф'),
-                        ('ё', '1', '!', 'ф', 'г', 'ш'),
-                        ('*', '.', 'ц', 'у', 'ф')]}
-
-juken = {}
-diktor = {}
-zubachew = {}
-skoropis = {}
-for key, values in keylout_dd.items():
-    juken[key] = values[0]  # Первое значение
-    diktor[key] = values[1]  # Второе значение, если есть
-    zubachew[key] = values[2]  # Третье значение, если есть
-    skoropis[key] = values[3]  # Четвертое значение, если есть
-
-with open(filename, 'r', encoding='utf-8') as file:
-    filef = [line.strip() for line in file.readlines()]
-    # Создание списка символов из файла
-bukvi1 = [char.lower() for lexema in filef for char in lexema if
-          any(char.lower() == value for values in juken.values() for value in values)]
-bukvi2 = [char.lower() for lexema in filef for char in lexema if
-          any(char.lower() == value for values in diktor.values() for value in values)]
-bukvi3 = [char.lower() for lexema in filef for char in lexema if
-          any(char.lower() == value for values in zubachew.values() for value in values)]
-bukvi4 = [char.lower() for lexema in filef for char in lexema if
-          any(char.lower() == value for values in skoropis.values() for value in values)]
-
-
-async def count_symbols_async(symbol_counter, layout, bukvi):
-    loop = asyncio.get_event_loop()
-    return await loop.run_in_executor(None, symbol_counter.count_symbols, layout, bukvi)
 
 
 def main():
+    filename = (r'C:\sortchbukw.csv', )
+    labels = ('йцукен', 'diktor', 'zubachew', 'skoropis')
+    symbols_with_shift = (('!', '"', '№', ';', '%', ':', '?',
+                           '*', '(', ')', '_', '+', '/', ','),
+                          ('№', '%', ':', ';', '-', '"', '(',
+                           ')', '+', 'ъ', '?', '!', '_'),
+                          ('!', '"', '№', ';', '%', ':', '?',
+                           '*', '(', ')', '_', '+', '/'),
+                          ('', ))
+    homerows = (('ф', 'ы', 'в', 'а', 'о', 'л', 'д', 'ж', ' '),
+                ('у', 'и', 'е', 'о', 'н', 'т', 'с', 'р', ' '),
+                ('г', 'и', 'е', 'о', 'т', 'с', 'н', 'з', ' '),
+                ('у', 'и', 'е', 'о', 'н', 'т', 'с', 'р', ' '),
+                (' ', ))
+    keylout_dd = {'rfi1б': [(' ', ),
+                            (' ', ),
+                            (' ', ),
+                            (' ', )],
+                  'rfi5м': [('+', '_', ')', '-', '=', 'з', 'х',
+                             'ъ', 'ж', 'э'),
+                            ('+', '_', ')', '0', '*', '=', 'ч',
+                             'ш', 'щ', 'р', 'й', 'ж'),
+                            ('0', ')', '-', '_', '=', '+', 'х',
+                             'ц', 'щ', '/', 'з', 'ж', 'ч'),
+                            (')', '_', '"',
+                             'ч', 'ш', 'щ', 'р', 'й', 'ж')],
+                  'rfi4б': [('(', '0', 'щ', 'д', 'ю', '.'),
+                            ('(', '9', 'д', 'с', 'г'),
+                            ('9', '(', 'п', 'н', 'к'),
+                            ('(', 'д', 'с', 'г')],
+                  'rfi3с': [('*', '8', '9', 'ш', 'л', 'б'),
+                            ('"', '8', 'к', 'т', 'п'),
+                            ('8', '*', 'р', 'с', 'в'),
+                            ('к', 'т', 'п')],
+                  'rfi2у': [('6', '?', '7', 'н', 'г', 'р', 'о',
+                             'т', 'ь'),
+                            ('6', '-', ';', '7', 'з', 'в', 'л',
+                             'н', 'м', 'б'),
+                            ('7', '?', '6', ':', 'й', 'л', 'б',
+                             'м', 'т', 'д'),
+                            ('-', 'з', 'в', 'л', 'н', 'б', 'м')],
+                  'lfi2у': [('4', '%', '5', 'к', 'е', 'а', 'п',
+                             'м', 'и'),
+                            ('4', '5', '.', ',', '!', '?', 'а',
+                             'ы', 'ю', 'о'),
+                            ('5', '4', '%', ';', ',', '.', 'я',
+                             'о', 'у', 'э'),
+                            ('?', '!', ',', '.', 'о', 'а',
+                             'ы', 'ю')],
+                  'lfi3с': [('№', '3', 'у', 'в', 'с'),
+                            ('3', '№', 'я', 'е', 'х'),
+                            ('3', '№', 'а', 'е', 'ю'),
+                            ('ъ', 'я', 'е', 'х')],
+                  'lfi4б': [('"', '2', 'ц', 'ы', 'ч'),
+                            ('2', 'ь', 'ъ', 'и', 'э'),
+                            ('2', '"', 'ы', 'и', 'ь', 'ъ'),
+                            ('ё', 'ь', 'и', 'э')],
+                  'lfi5м': [('!', 'ё', 'й', 'ф', 'я', '1'),
+                            ('ё', '1', 'ц', 'у', 'ф'),
+                            ('ё', '1', '!', 'ф', 'г', 'ш'),
+                            ('*', '.', 'ц', 'у', 'ф')]}
+
     symbol_counter = TextAnalyzer(filename, keylout_dd,
                                   symbols_with_shift, homerows)
-
-    # Запускаем асинхронные задачи
-    final_loads1 = symbol_counter.count_symbols(juken, bukvi1)
-    final_loads2 = symbol_counter.count_symbols(diktor, bukvi2)
-    final_loads3 = symbol_counter.count_symbols(zubachew, bukvi3)
-    final_loads4 = symbol_counter.count_symbols(skoropis, bukvi4)
-    print(final_loads1)
-    print(final_loads2)
-    print(final_loads3)
-    print(final_loads4)
+    final_loads = symbol_counter.count_symbols()
+    loads_for_ccombol = final_loads[2]
+    loads_for_combol = final_loads[3]
+    loads_for_ccombor = final_loads[0]
+    loads_for_combor = final_loads[1]
+    print(f"{'':<20} {'Правая':<30} {'Левая':<30}")
+    print(
+        f"{'Одной рукой':<20} {' '.join(f'{loads_for_ccombor[i]}' for i in range(4)):<30} \
+{' '.join(f'{loads_for_ccombol[i]}' for i in range(4)):<30}")
+    print(
+        f"{'Удобная':<20} {' '.join(f'{loads_for_combor[i]}' for i in range(4)):<30} \
+{' '.join(f'{loads_for_combol[i]}' for i in range(4)):<30}")
 
 
 if __name__ == "__main__":
-    t1 = monotonic()
     main()
-    t2 = monotonic()
-    print(t2 - t1)
--- text_analyzer.py
+++ text_analyzer.py
@@ -1,111 +1,227 @@
-def get_finger(char, layout):
-    for finger, values in layout.items():
-        if char in values:
-            return finger
-    return None
+import re
 
 
 class TextAnalyzer:
     def __init__(self, filename, symbols, shifts, homekeys):
+        """
+        Инициализация класса TextAnalyzer.
+
+        :param filename: Путь к файлу, который будет анализироваться.
+        :param symbols: Словарь, содержащий символы
+        и соответствующие им раскладки.
+        :param shifts: Список сдвигов для анализа символов.
+        """
+        self.previous_load = None
+        self.distant_symbol = [('х', 'ъ', 'ё'),
+                               ('ш', 'щ', 'ё'),
+                               ('ц', 'щ', 'ё'),
+                               ('ш', 'щ', '')]
+        self.digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
         self.filename = filename
+        self.symbols = symbols
         self.shifts = shifts
         self.homekeys = homekeys
-        self.finger_counts = [{finger: 0 for finger in symbols.keys()} for _ in range(2)]
-        self.previous_load = None
+        self.finger_fines = [{finger: 0 for finger in symbols.keys()} for _ in range(4)]
+        self.finger_loads = [{finger: 0 for finger in symbols.keys()} for _ in range(4)]
 
-    def is_convenient(self, current_finger, previous_finger, hand):
-        if current_finger is None or previous_finger is None:
-            return False
+    def find_finger(self, char):
+        """
+        Находит, каким пальцем следует печатать данный
+        символ в каждой из раскладок.
 
-        same_hand = (current_finger.startswith('lfi') and hand == 0) or (current_finger.startswith('rfi') and hand == 1)
-        current_key_num = int(current_finger[3]) if len(current_finger) > 3 and current_finger[3].isdigit() else None
-        previous_key_num = int(previous_finger[3]) if len(previous_finger) > 3 and previous_finger[
-            3].isdigit() else None
+        :param char: Символ, для которого нужно определить палец.
+        :return: Список, содержащий два элемента: палец в каждой раскладке.
+        """
+        i = [None, None, None, None]
+        # print(self.symbols.items())
+        for finger, layouts in self.symbols.items():
+            for layout in layouts:
+                if char in layout:
+                    if layouts.index(layout) == 3:
+                        i[3] = finger
+                    if layouts.index(layout) == 2:
+                        i[2] = finger
+                    if layouts.index(layout) == 1:
+                        i[1] = finger
+                    if layouts.index(layout) == 0:
+                        i[0] = finger
+        return i
 
-        decreasing_number = (
-                current_key_num is not None and previous_key_num is not None and current_key_num < previous_key_num)
+    def is_convenientr(self, current_load):
+        """
+        Определяет, является ли нажатие удобным для каждой раскладки.
+        :return: Список из True и False для каждой раскладки.
+        """
+        if self.previous_load is None:
+            return [False, False, False, False]
+        results2 = []  # Список для хранения результатов для каждой раскладки
 
-        return same_hand and decreasing_number
+        for i in range(4):  # Проверяем для каждой раскладки
+            current_finger = current_load[i]
+            previous_finger = self.previous_load[i]
 
-    def count_symbols(self, layout, bukvi):
-        hand_counts_pairs = [0,
-                             0]  # Для левой и правой руки (hand_counts_pairs[0] - левая, hand_counts_pairs[1] - правая)
-        correct_sequence_counts_pairs = [0, 0]  # Для правильной последовательности пар
-
-        hand_counts_triples = [0, 0]  # Для левой и правой руки для троек
-        correct_sequence_counts_triples = [0, 0]  # Для правильной последовательности троек
-
-        # Кэширование пальцев
-        fingers_cache = {}
-
-        # Обработка символов
-        for i in range(len(bukvi)):
-            char = bukvi[i]
-
-            if char in [' ', '\n']:
+            if current_finger is None or previous_finger is None:
+                results2.append(False)  # Если палец не найден, добавляем False
                 continue
 
-            # Получаем палец для текущего символа, используя кэш
-            if char not in fingers_cache:
-                fingers_cache[char] = get_finger(char, layout)
+            # Проверяем, использовалась ли одна рука
+            same_hand = (current_finger.startswith('rfi') and
+                         previous_finger.startswith('rfi'))
+            results2.append(same_hand)
+        return results2
 
-            current_finger = fingers_cache[char]
-            current_hand = 0 if current_finger.startswith('lfi') else 1 if current_finger.startswith('rfi') else None
+    def is_convenient_pressr(self, current_load):
+        """
+        Определяет, является ли нажатие удобным для каждой раскладки.
+        :return: Список из True и False для каждой раскладки.
+        """
+        if self.previous_load is None:
+            return [False, False, False, False]
+        results1 = []  # Список для хранения результатов для каждой раскладки
 
-            # Обработка пар
-            if i < len(bukvi) - 1:
-                next_char = bukvi[i + 1]
-                if next_char in [' ', '\n']:
-                    continue
+        for i in range(4):  # Проверяем для каждой раскладки
+            current_finger = current_load[i]
+            previous_finger = self.previous_load[i]
 
-                if next_char not in fingers_cache:
-                    fingers_cache[next_char] = get_finger(next_char, layout)
+            if current_finger is None or previous_finger is None:
+                results1.append(False)  # Если палец не найден, добавляем False
+                continue
 
-                next_finger = fingers_cache[next_char]
-                next_hand = 0 if next_finger.startswith('lfi') else 1 if next_finger.startswith('rfi') else None
+            # Проверяем, использовалась ли одна рука
+            same_hand = (current_finger.startswith('rfi') and
+                         previous_finger.startswith('rfi'))
+            # Получаем цифры из ключей
+            current_key_num = int(current_finger[3]) \
+                if len(current_finger) > 3 and current_finger[3].isdigit() \
+                else None
+            previous_key_num = int(previous_finger[3]) \
+                if len(previous_finger) > 3 and previous_finger[3].isdigit() \
+                else None
 
-                # Увеличиваем счетчики для одной руки
-                if current_hand is not None and current_hand == next_hand:
-                    hand_counts_pairs[current_hand] += 1
+            # Проверяем, уменьшается ли цифра
+            decreasing_number = (current_key_num is not None and
+                                 previous_key_num is not None and
+                                 current_key_num < previous_key_num)
 
-                # Проверка на правильную последовательность
-                if current_hand is not None and self.is_convenient(next_finger, current_finger, current_hand):
-                    correct_sequence_counts_pairs[current_hand] += 1
+            # Если нажатие удобное, добавляем True, иначе False
+            results1.append(same_hand and decreasing_number)
+        return results1  # Возвращаем список результатов
 
-            # Обработка троек
-            if i < len(bukvi) - 2:
-                next_char = bukvi[i + 1]
-                third_char = bukvi[i + 2]
+    def is_convenientl(self, current_load):
+        """
+        Определяет, является ли нажатие удобным для каждой раскладки.
+        :return: Список из True и False для каждой раскладки.
+        """
+        if self.previous_load is None:
+            return [False, False, False, False]
+        results = []  # Список для хранения результатов для каждой раскладки
 
-                if next_char in [' ', '\n'] or third_char in [' ', '\n']:
-                    continue
+        for i in range(4):  # Проверяем для каждой раскладки
+            current_finger = current_load[i]
+            previous_finger = self.previous_load[i]
 
-                if next_char not in fingers_cache:
-                    fingers_cache[next_char] = get_finger(next_char, layout)
+            if current_finger is None or previous_finger is None:
+                results.append(False)  # Если палец не найден, добавляем False
+                continue
 
-                if third_char not in fingers_cache:
-                    fingers_cache[third_char] = get_finger(third_char, layout)
+            # Проверяем, использовалась ли одна рука
+            same_hand = (current_finger.startswith('lfi') and
+                         previous_finger.startswith('lfi'))
+            results.append(same_hand)
+        return results
 
-                next_finger = fingers_cache[next_char]
-                next_hand = 0 if next_finger.startswith('lfi') else 1 if next_finger.startswith('rfi') else None
-                third_finger = fingers_cache[third_char]
-                third_hand = 0 if third_finger.startswith('lfi') else 1 if third_finger.startswith('rfi') else None
+    def is_convenient_pressl(self, current_load):
+        """
+        Определяет, является ли нажатие удобным для каждой раскладки.
+        :return: Список из True и False для каждой раскладки.
+        """
+        if self.previous_load is None:
+            return [False, False, False, False]
+        results = []  # Список для хранения результатов для каждой раскладки
 
-                # Увеличиваем счетчики для одной руки
-                if current_hand is not None and current_hand == next_hand == third_hand:
-                    hand_counts_triples[current_hand] += 1
+        for i in range(4):  # Проверяем для каждой раскладки
+            current_finger = current_load[i]
+            previous_finger = self.previous_load[i]
 
-                # Проверка на правильную последовательность
-                if current_hand is not None and self.is_convenient(next_finger, current_finger,
-                                                                   current_hand) and self.is_convenient(third_finger,
-                                                                                                        next_finger,
-                                                                                                        current_hand):
-                    correct_sequence_counts_triples[current_hand] += 1
+            if current_finger is None or previous_finger is None:
+                results.append(False)  # Если палец не найден, добавляем False
+                continue
 
-        return [  # [пары для левой руки, пары для правой, последовательность левой, правой, тройки левой, правой,
-            # последовательность левой, правой]
-            hand_counts_pairs[0], hand_counts_pairs[1],
-            correct_sequence_counts_pairs[0], correct_sequence_counts_pairs[1],
-            hand_counts_triples[0], hand_counts_triples[1],
-            correct_sequence_counts_triples[0], correct_sequence_counts_triples[1]
-        ]
+            # Проверяем, использовалась ли одна рука
+            same_hand = (current_finger.startswith('lfi') and
+                         previous_finger.startswith('lfi'))
+            # Получаем цифры из ключей
+            current_key_num = int(current_finger[3]) \
+                if len(current_finger) > 3 and current_finger[3].isdigit() \
+                else None
+            previous_key_num = int(previous_finger[3]) \
+                if len(previous_finger) > 3 and previous_finger[3].isdigit() \
+                else None
+
+            # Проверяем, уменьшается ли цифра
+            decreasing_number = (current_key_num is not None and
+                                 previous_key_num is not None and
+                                 current_key_num < previous_key_num)
+
+            # Если нажатие удобное, добавляем True, иначе False
+            results.append(same_hand and decreasing_number)
+        return results  # Возвращаем список результатов
+
+    def count_symbols(self):
+        """
+        Производит подсчет количества нажатий для каждого пальца
+        :return: Два словаря, с количеством нажатий для каждогго пальца
+        """
+        combor = [0] * 4
+        ccombor = [0] * 4
+        combol = [0] * 4
+        ccombol = [0] * 4
+        for filepath in self.filename:
+            try:
+                with open(filepath, 'r', encoding='utf-8') as file:
+                    for line in file:
+                        line = re.findall(r'[a-zA-Zа-яА-Я]', line)
+                        length = len(line)
+                        for i in range(length):
+                            for j in range(i + 1, length):
+                                char1 = line[i]
+                                char2 = line[j]
+                                self.previous_load = self.find_finger(char1.lower())
+                                t2 = self.find_finger(char2.lower())
+
+                                for k in range(4):
+                                    if self.is_convenient_pressr(t2)[k]:
+                                        combor[k] += 1
+                                    if self.is_convenientr(t2)[k]:
+                                        ccombor[k] += 1
+                                    if self.is_convenient_pressl(t2)[k]:
+                                        combol[k] += 1
+                                    if self.is_convenientl(t2)[k]:
+                                        ccombol[k] += 1
+                final = [ccombor, combor, ccombol, combol]
+                return final
+
+            except FileNotFoundError:
+                print("Файл не найден.")
+            except IOError:
+                print("Ошибка ввода-вывода при работе с файлом.")
+            # except Exception as e:
+            #     print(f"Произошла ошибка: {e}")
+
+    def display_counts(self):
+        """
+        Выводит на экран количество символов, напечатанных каждым пальцем.
+
+        :return: None
+        """
+        for symbol, count in self.finger_load.items():
+            print(f"'{symbol}': {count}")
+        print('---------------------')
+        for symbol, count in self.finger_load2.items():
+            print(f"'{symbol}': {count}")
+        print('---------------------')
+        for symbol, count in self.finger_load3.items():
+            print(f"'{symbol}': {count}")
+        print('---------------------')
+        for symbol, count in self.finger_load4.items():
+            print(f"'{symbol}': {count}")
